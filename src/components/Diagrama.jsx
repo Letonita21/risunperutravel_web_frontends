8b8a163384b45c9636979916526d115a3cba8276
will <will.leton21@gmail.com>
2026-03-05 10:27:08 -0500

    cambios en la imagen del tours

--- src/components/Diagrama.jsx
+++ src/components/Diagrama.jsx
@@ -3,23 +3,20 @@ import React from "react";
 
 const Diagrama = ({ lang, titulo, tour, contenido }) => {
   return (
-    <div className="w-full flex flex-col items-start">
-      <h2 className="text-2xl font-semibold text-orange-600 mb-2">{titulo}</h2>
-
-      <p className="text-gray-700 text-lg mb-4 text-start max-w-3xl">
-        {contenido}
-      </p>
-
-      <div className="w-full flex justify-start">
-        <Image
-          src={`/responsable/${lang}.webp`}
-          alt="turismo responsable"
-          priority
-          width={800}
-          height={600}
-          className="w-full max-w-3xl h-auto rounded-lg shadow-lg"
-        />
-      </div>
+    <div style={{ width: "100%" }}>
+      <h2 className="text-2xl font-semibold text-orange-600 flex items-center gap-0.5 mb-2">
+        {titulo}
+      </h2>
+      <p className="text-gray-700 text-lg mb-4">{contenido}</p>
+      <Image
+        src={`/responsable/${lang}.webp`}
+        alt="turismo responsable"
+        priority
+        width={700}
+        height={600}
+        className="rounded-lg drop-shadow-lg drop-shadow-gray-600"
+        sizes="100vw"
+      />
     </div>
   );
 };
