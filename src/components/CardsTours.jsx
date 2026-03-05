81ff59a111e4f23799d30c33e684515b071a1359
will <will.leton21@gmail.com>
2026-03-05 10:58:59 -0500

    cardprecio

--- src/components/CardsTours.jsx
+++ src/components/CardsTours.jsx
@@ -1,8 +1,14 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Transition } from "@headlessui/react";
 import Link from "next/link";
-import { ClockIcon, HandRightIcon, LugarIcon, ShoeIcon, MountainIcon } from '@/icons';
+import {
+  ClockIcon,
+  HandRightIcon,
+  LugarIcon,
+  ShoeIcon,
+  MountainIcon,
+} from "@/icons";
 
 const CardsTours = ({ tours, dict }) => {
   const [current, setCurrent] = useState(0);
@@ -35,38 +41,66 @@ const CardsTours = ({ tours, dict }) => {
               leave="transition transform duration-2000"
               leaveFrom="translate-y-0 opacity-100"
               leaveTo="-translate-y-full opacity-0"
-              className="absolute top-0 left-0 w-full h-full flex flex-col md:flex-row">
-
+              className="absolute top-0 left-0 w-full h-full flex flex-col md:flex-row"
+            >
               <div
                 className="relative w-full max-w-sm h-[410px] overflow-hidden shadow-lg group rounded-xl drop-shadow-md drop-shadow-gray-500"
                 onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)}>
+                onMouseLeave={() => setHovered(false)}
+              >
                 <img
                   src={`${url}${tour.imagen_portada}`}
                   alt={tour.nombre}
                   title={tour.nombre}
-                  loading='lazy'
+                  loading="lazy"
                   className="w-full h-full object-cover transform group-hover:scale-110 transition duration-1000"
                 />
-                <div className="absolute top-5 left-2 bg-orange-500 text-white font-semibold px-5 py-0.5 rounded-md text-md shadow-md shadow-gray-800">
-                  {dict.desde}: <p className='text-2xl font-bold drop-shadow-md -mt-2 drop-shadow-gray-600'>${tour.precio_enganche}</p>
+                <div className="absolute top-5 left-2 bg-[#A30923] text-white font-semibold px-5 py-0.5 rounded-md text-md shadow-md shadow-gray-800">
+                  {dict.desde}:{" "}
+                  <p className="text-2xl font-bold drop-shadow-md -mt-2 drop-shadow-gray-600">
+                    ${tour.precio_enganche}
+                  </p>
                 </div>
                 <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/40 to-transparent opacity-0 group-hover:opacity-100 transition duration-500"></div>
                 <div
-                  className={`absolute left-0 w-full p-4 text-white z-10 transition-all duration-500 ${hovered ? "bottom-20" : "bottom-0"
-                    }`}>
-                  <p className="flex gap-1 text-lg items-center font-semibold opacity-90 drop-shadow-sm drop-shadow-gray-800">{tour.dificultad} <ShoeIcon /> / {tour.picos} <MountainIcon /></p>
-                  <p className="flex gap-1 text-lg items-center font-semibold opacity-90 drop-shadow-sm drop-shadow-gray-800"><ClockIcon /> {tour.duracion} {tour.duracion > "1" ? dict.dias : dict.dia}</p>
-                  <p className="flex gap-1 text-lg items-center font-semibold opacity-90 drop-shadow-sm drop-shadow-gray-800"><LugarIcon /> {tour.departamento}</p>
-                  <h2 className="text-xl font-bold drop-shadow-sm drop-shadow-gray-800">{tour.nombre}</h2>
+                  className={`absolute left-0 w-full p-4 text-white z-10 transition-all duration-500 ${
+                    hovered ? "bottom-20" : "bottom-0"
+                  }`}
+                >
+                  <p className="flex gap-1 text-lg items-center font-semibold opacity-90 drop-shadow-sm drop-shadow-gray-800">
+                    {tour.dificultad} <ShoeIcon /> / {tour.picos}{" "}
+                    <MountainIcon />
+                  </p>
+                  <p className="flex gap-1 text-lg items-center font-semibold opacity-90 drop-shadow-sm drop-shadow-gray-800">
+                    <ClockIcon /> {tour.duracion}{" "}
+                    {tour.duracion > "1" ? dict.dias : dict.dia}
+                  </p>
+                  <p className="flex gap-1 text-lg items-center font-semibold opacity-90 drop-shadow-sm drop-shadow-gray-800">
+                    <LugarIcon /> {tour.departamento}
+                  </p>
+                  <h2 className="text-xl font-bold drop-shadow-sm drop-shadow-gray-800">
+                    {tour.nombre}
+                  </h2>
                 </div>
+                {/* Botón */}
                 <div
-                  className={`absolute bottom-0 left-0 w-full p-4 text-white transform transition-all duration-500 ${hovered ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
-                    }`}>
+                  className={`absolute bottom-0 left-0 w-full p-5 transition-all duration-500 ${
+                    hovered
+                      ? "opacity-100 translate-y-0"
+                      : "opacity-0 translate-y-6"
+                  }`}
+                >
                   <Link
-                    className="cursor-pointer flex gap-2 mt-3 px-4 py-2 bg-orange-500 hover:bg-orange-600 rounded-lg font-semibold text-sm shadow"
-                    href={`/${tour.idioma}/${tour.tipo}/${tour.slug}`}>
-                    <HandRightIcon />{dict.masDetalles}
+                    href={`/${tour.idioma}/${tour.tipo}/${tour.slug}`}
+                    className="flex items-center justify-center gap-2 w-full
+                bg-[#A30923] hover:bg-[#87071d]
+                text-white font-semibold
+                py-2.5 rounded-xl
+                shadow-lg hover:shadow-xl
+                transition"
+                  >
+                    <HandRightIcon />
+                    {dict.masDetalles}
                   </Link>
                 </div>
               </div>
@@ -74,22 +108,24 @@ const CardsTours = ({ tours, dict }) => {
           ))}
         </div>
 
-        <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 flex gap-3">
+        {/* DOTS */}
+        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 flex gap-3">
           {tours.map((_, index) => (
             <button
               key={index}
-              aria-label={`boton cardTours ir a tarjeta${index + 1}`}
+              aria-label={`boton cardTours ir a tarjeta ${index + 1}`}
               onClick={() => setCurrent(index)}
-              className={`w-4 h-4 rounded-full border-2 transition-all duration-300 ${current === index
-                ? "bg-orange-400 border-orange-400 scale-110"
-                : "bg-white border-gray-400 hover:bg-gray-300"
-                }`}
+              className={`h-3 rounded-full transition-all duration-300 ${
+                current === index
+                  ? "w-8 bg-[#A30923]"
+                  : "w-3 bg-gray-300 hover:bg-gray-400"
+              }`}
             ></button>
           ))}
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default CardsTours
+export default CardsTours;
